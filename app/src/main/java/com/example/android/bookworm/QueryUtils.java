b7f8bbf2e8abd26466f37da8dabc0640209f22f2
Muir Halleron <muir.halleron@gmail.com>
2017-06-02 07:57:22 +0100

    Pull out string keys into private static final constants to keep them all in one place

--- app/src/main/java/com/example/android/bookworm/QueryUtils.java
+++ app/src/main/java/com/example/android/bookworm/QueryUtils.java
@@ -28,6 +28,14 @@ public final class QueryUtils {
     // Tag for the log messages
     public static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
+    // Constant keys
+    private static final String KEY_ITEMS = "items";
+    private static final String KEY_VOLUME_INFO = "volumeInfo";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_SUBTITLE = "subtitle";
+    private static final String KEY_AUTHORS = "authors";
+    
+
     // Returns new URL object from the given string URL
     private static URL createUrl(String stringUrl) {
         URL url = null;
@@ -129,7 +137,7 @@ public final class QueryUtils {
              * Extract the JSONArray associated with the key called "items", which represents
              * a list of features (or books)
              */
-            JSONArray bookArray = baseJsonResponse.getJSONArray("items");
+            JSONArray bookArray = baseJsonResponse.getJSONArray(KEY_ITEMS);
 
             /**
              * for each book in the bookArray, create an {@link Book} object.
@@ -143,24 +151,24 @@ public final class QueryUtils {
                  * For a given book, extract the JSONObjects associated with the key
                  * "volumeInfo", which represents a list of information for that book
                  */
-                JSONObject volumeInfo = currentBook.getJSONObject("volumeInfo");
+                JSONObject volumeInfo = currentBook.getJSONObject(KEY_VOLUME_INFO);
 
                 // Extract "title" under "volumeInfo" for title
                 String title = "";
-                if (volumeInfo.has("title")) {
-                    title = volumeInfo.getString("title");
+                if (volumeInfo.has(KEY_TITLE)) {
+                    title = volumeInfo.getString(KEY_TITLE);
                 }
 
                 // Extract "subtitle" under "volumeInfo" for subtitle
                 String subtitle = "";
-                if (volumeInfo.has("subtitle"))
-                    subtitle = volumeInfo.getString("subtitle");
+                if (volumeInfo.has(KEY_SUBTITLE))
+                    subtitle = volumeInfo.getString(KEY_SUBTITLE);
 
 
                 // Extract "authors" under "volumeInfo" for author
                 String authors = "";
-                if (volumeInfo.has("authors")) {
-                    JSONArray authorsArray = volumeInfo.getJSONArray("authors");
+                if (volumeInfo.has(KEY_AUTHORS)) {
+                    JSONArray authorsArray = volumeInfo.getJSONArray(KEY_AUTHORS);
                     authors = authorsArray.toString().replace("[", "").replace("]", "");
                 }
 
